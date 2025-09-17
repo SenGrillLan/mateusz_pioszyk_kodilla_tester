22b63a8c88f6f0a2e25d52e99fb552d2b3313c05
Mateusz Pioszyk <m.pioszyk@gmail.com>
2025-09-17 18:48:56 +0200

    initial version

---
+++ kodilla-intro/src/main/java/LeapYear.java
@@ -0,0 +1,17 @@
+public class LeapYear {
+    public static void main(String[] args) {
+        int firstNumber =1237;
+
+        if (firstNumber%4 == 0 ) {
+            System.out.println("Rok jest przestepny");
+        } else if (firstNumber%4 > 0){
+            System.out.println("Rok nie jest przestepny");
+        } else if (firstNumber%100 > 0){
+            System.out.println("Rok jest przestepny");
+        } else if (firstNumber % 400 == 0){
+        System.out.println("Rok jest przestepny");
+    }
+    }
+
+
+}
---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,16 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String testWords = "Testowo testowy tekst";
+        int averageSpeed = 170;
+        double millingSpeed = 0.345;
+        char letterTest = 'D';
+        boolean itIsEassy = false;
+        float boChce = 1.666F;
+        System.out.println(testWords);
+        System.out.println(itIsEassy);
+        System.out.println(letterTest);
+        System.out.println(averageSpeed);
+        System.out.println(millingSpeed);
+        System.out.println(boChce);
+    }
+}
